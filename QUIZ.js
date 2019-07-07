d928c8c8bf5f072106aa7625a417c1f899bc6746
Lucca211 <51926270+Lucca211@users.noreply.github.com>
2019-07-07 15:54:57 -0300

    Update QUIZ.js

--- QUIZ.js
+++ QUIZ.js
@@ -1,94 +1,94 @@
 var myQuestions = [
-	{
-		question: "Qual lei da física não existe?",
-		answers: {
-			a: 'Inércia',
-			b: 'Gravidade',
-			c: 'Força'
-		},
-		correctAnswer: 'b'
+{
+	question: "Qual lei da física não existe?",
+	answers: {
+		a: 'Inércia',
+		b: 'Gravidade',
+		c: 'Força'
 	},
-	{
-		question: "Qual é o maior atual pesquisador Terraplanista?",
-		answers: {
-			a: 'Eu e Ela Curiosa',
-			b: 'Olavo de Carvalho',
-			c: 'Professor Terra Plana',
-		},
-		correctAnswer: 'c'
+	correctAnswer: 'b'
+},
+{
+	question: "Qual é o maior atual pesquisador Terraplanista?",
+	answers: {
+		a: 'Eu e Ela Curiosa',
+		b: 'Olavo de Carvalho',
+		c: 'Professor Terra Plana',
 	},
-	{
-		question: "Quem é o nosso maior rival?",
-		answers: {
-			a: 'ET Bilu',
-			b: 'NASA',
-			c: 'Russos',
-		},
-		correctAnswer: 'a'
+	correctAnswer: 'c'
+},
+{
+	question: "Quem é o nosso maior rival?",
+	answers: {
+		a: 'ET Bilu',
+		b: 'NASA',
+		c: 'Russos',
 	},
-	{
-		question: "Qual teoria rivaliza com a Terra Plana?",
-		answers: {
-			a: 'Terra Geóide',
-			b: 'Terra Convexa',
-			c: 'Terra Rosquinha',
-		},
-		correctAnswer: 'a'
+	correctAnswer: 'a'
+},
+{
+	question: "Qual teoria rivaliza com a Terra Plana?",
+	answers: {
+		a: 'Terra Geóide',
+		b: 'Terra Convexa',
+		c: 'Terra Rosquinha',
 	},
-	{
-		question: "Qual cientista mentiu em relação à física terrestre?",
-		answers: {
-			a: 'Isaac Newton',
-			b: 'Albert Einstein',
-			c: 'Fábio do Click Time',
-		},
-		correctAnswer: 'a'
+	correctAnswer: 'a'
+},
+{
+	question: "Qual cientista mentiu em relação à física terrestre?",
+	answers: {
+		a: 'Isaac Newton',
+		b: 'Albert Einstein',
+		c: 'Fábio do Click Time',
 	},
-	{
-		question: "Qual destas celebridades apoia a Terra Plana?",
-		answers: {
-			a: 'Isaque Milton',
-			b: 'Tila Tequila',
-			c: 'Paulo Kogos',
-		},
-		correctAnswer: 'b'
+	correctAnswer: 'a'
+},
+{
+	question: "Qual destas celebridades apoia a Terra Plana?",
+	answers: {
+		a: 'Isaque Milton',
+		b: 'Tila Tequila',
+		c: 'Paulo Kogos',
 	},
-	{
-		question: "Os dinossauros realmente existiram?",
-		answers: {
-			a: 'Sim',
-			b: 'Sim, porém depende',
-			c: 'Óbvio que não',
-		},
-		correctAnswer: 'c'
+	correctAnswer: 'b'
+},
+{
+	question: "Os dinossauros realmente existiram?",
+	answers: {
+		a: 'Sim',
+		b: 'Sim, porém depende',
+		c: 'Óbvio que não',
 	},
-	{
-		question: "Qual empresa não mente em relação à Terra Plana?",
-		answers: {
-			a: 'NASA',
-			b: 'FEIC',
-			c: 'URSS',
-		},
-		correctAnswer: 'b'
+	correctAnswer: 'c'
+},
+{
+	question: "Qual empresa não mente em relação à Terra Plana?",
+	answers: {
+		a: 'NASA',
+		b: 'FEIC',
+		c: 'URSS',
 	},
-	{
-		question: "Por que nossos sapatos são retos?",
-		answers: {
-			a: 'Superfície Plana da Terra',
-			b: 'Anti-Gravidade',
-			c: 'Convenção para a monopolização das empresas de sapatos',
-		},
-		correctAnswer: 'a'
+	correctAnswer: 'b'
+},
+{
+	question: "Por que nossos sapatos são retos?",
+	answers: {
+		a: 'Superfície Plana da Terra',
+		b: 'Anti-Gravidade',
+		c: 'Convenção para a monopolização das empresas de sapatos',
 	},
-	{
-		question: "Depois de todos estes dados, você acredita na Terra Plana?",
-		answers: {
-			a: 'Sou fraco, não',
-			b: 'SIM',
-			c: 'Não aguento tanta verdade na minha frente, pego minha ignorância e vou embora',
-		},
-		correctAnswer: 'b'
-	}
+	correctAnswer: 'a'
+},
+{
+	question: "Depois de todos estes dados, você acredita na Terra Plana?",
+	answers: {
+		a: 'Sou fraco, não',
+		b: 'SIM',
+		c: 'Não aguento tanta verdade na minha frente, pego minha ignorância e vou embora',
+	},
+	correctAnswer: 'b'
+}
 ];
 
 var quizContainer = document.getElementById('quiz');
@@ -99,82 +99,61 @@ generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
-	function showQuestions(questions, quizContainer){
-		// precisaremos de um lugar para armazenar a saída e as opções de resposta
-		var output = [];
-		var answers; // poderia ter dado outro nome, alem do mais, eu mesmo me confundi :/
+function showQuestions(questions, quizContainer){
+	var output = [];
+	var answers;
+	for(var i=0; i<questions.length; i++){
 
-		// para cada questão
-		for(var i=0; i<questions.length; i++){
+		answers = [];
 
-			// mas, primeiro resetamos a lista de questões
-			answers = [];
+		for(letter in questions[i].answers){
 
-			// e aqui faremos para cada resposta na questão.
-			for(letter in questions[i].answers){
-
-				// Aqui será escrito para html para usar o Radio.
-				answers.push(
-					'<label>'
-						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-					//	+ letter + ') ' vou deixar a letra comentada, porque achei melhor assim
-						+ questions[i].answers[letter]
-					+ '</label>'
-				);
-			}
-
-			// add this question and its answers to the output
-			output.push(
-				'<div class="question">' + questions[i].question + '</div>'
-				+ '<div class="answers">' + answers.join('') + '</div>'
+			answers.push(
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ questions[i].answers[letter]
+				+ '</label>'
 			);
 		}
 
-		// finally combine our output list into one string of html and put it on the page
-		quizContainer.innerHTML = output.join('');
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
 	}
 
+	quizContainer.innerHTML = output.join('');
+}
 
-	function showResults(questions, quizContainer, resultsContainer){
+function showResults(questions, quizContainer, resultsContainer){
 
+	var answerContainers = quizContainer.querySelectorAll('.answers');
 
-		var answerContainers = quizContainer.querySelectorAll('.answers');
+	var userAnswer = '';
+	var numCorrect = 0;
 
+	for(var i=0; i<questions.length; i++){
 
-		var userAnswer = '';
-		var numCorrect = 0;
+		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
 
+		if(userAnswer===questions[i].correctAnswer){
 
-		for(var i=0; i<questions.length; i++){
+			numCorrect++;
 
-			// agora ele vai virar a letra que esta marcada.
-			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-
-
-
-			if(userAnswer===questions[i].correctAnswer){
-
-				numCorrect++;
-
-
-				answerContainers[i].style.color = 'lightgreen';
-			}
-
-			else{
-
-				answerContainers[i].style.color = 'red';
-			}
+			answerContainers[i].style.color = 'lightgreen';
 		}
 
+		else{
 
-			resultsContainer.innerHTML = 'O número de questões certas foram: ' + numCorrect + ' de ' + questions.length;
+			answerContainers[i].style.color = 'red';
 		}
-
-	// show questions right away
-	showQuestions(questions, quizContainer);
-
-	// on submit, show results
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
 	}
+
+		resultsContainer.innerHTML = 'O número de questões certas foram: ' + numCorrect + ' de ' + questions.length;
+	}
+showQuestions(questions, quizContainer);
+
+submitButton.onclick = function(){
+	showResults(questions, quizContainer, resultsContainer);
+}
 }
